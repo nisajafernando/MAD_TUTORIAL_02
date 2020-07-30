1cfec5a7482c47424e8723b252bfb1b202bad11d
NISAJA FERNANDO <nisajafernando30@gmail.com>
2020-07-30 17:17:51 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView TxtVMsg2 = findViewById(R.id.TvMsg2);
+        TxtVMsg2.setText(R.string.Msg2);
+
+      Log.i( "Lifecycle", "OnCreate() invoked ....");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(   "Lifecycle" ,"onStart() invoked...");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onRestart();
+        Log.i(   "Lifecycle" ,"onResume() invoked...");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(   "Lifecycle" ,"onPause() invoked...");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(   "Lifecycle" ,"onStop() invoked...");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(   "Lifecycle" ,"onDestroy() invoked...");
+    }
+}
